bad8b291bce1463d3565d004ba8c703afcb7f017
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-06-17 16:02:04 +0900

    feat: add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,16 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { Connection } from 'typeorm';
+
 
 @Module({
-  // imports: [
-  //   TypeOrmModule.forRoot(),
-  // ],
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
