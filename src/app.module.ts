8a4d92838bdae18d36538c89c86bb1b8d33621ef
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-06-17 23:27:59 +0900

    feat: add middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
-import { Module } from '@nestjs/common'
+import { Module, NestModule, RequestMethod, MiddlewareConsumer } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Connection } from 'typeorm'
 import { UserModule } from './user/user.module'
+import { FirebaseAuthMiddleware } from './firebase'
 
 @Module({
   imports: [TypeOrmModule.forRoot(), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(FirebaseAuthMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
   constructor(private readonly connection: Connection) {}
 }
