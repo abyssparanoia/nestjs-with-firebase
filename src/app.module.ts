8dd1e051a4fd618c4eaa184e825f5da78f00c343
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-06-17 15:21:04 +0900

    fix: remove type orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(),
-  ],
+  // imports: [
+  //   TypeOrmModule.forRoot(),
+  // ],
+  imports: [],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
