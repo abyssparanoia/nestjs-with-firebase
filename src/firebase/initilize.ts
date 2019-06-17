de0b87ea5e3267b0bdbc4f1b8f0f70c38000b445
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-06-18 00:02:46 +0900

    fix: replcae

--- src/firebase/initilize.ts
+++ src/firebase/initilize.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin'
 export default admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.PROJECT_ID,
-    privateKey: process.env.PRIVATE_KEY,
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.CLIENT_EMAIL,
   }),
 })
